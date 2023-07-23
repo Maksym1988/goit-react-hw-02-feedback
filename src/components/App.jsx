2cade0a6d2a1407f4bb09825cb028b660015f8e0
Maksym1988 <splien1988@gmail.com>
2023-07-23 20:48:52 +0100

    Add Render first Sections

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,26 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React, { Component } from 'react';
+import Section from './Section/Section';
+import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
+import Statistics from './Statistics/Statistics';
+import css from './App.module.css';
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+    return (
+      <div className={css.container}>
+        <Section title="Please leave feedback">
+          <FeedbackOptions options={Object.keys(this.state)} />
+        </Section>
+        <Section title="Statistics"></Section>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+import css from './FeedbackOptions.module.css';
+
+const FeedbackOptions = ({ options, onLeaveFeedback }) => {
+  return (
+    <ul>
+      {options.map(option => {
+        return (
+          <li key={option}>
+            <button type="button" key={option} value={option}>
+              {option}
+            </button>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+FeedbackOptions.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string.isRequired),
+};
+
+export default FeedbackOptions;
