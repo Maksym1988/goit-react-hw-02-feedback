d8304086d110ddb3eda8e91707f557101bf5ff5e
Maksym1988 <splien1988@gmail.com>
2023-07-26 20:10:47 +0100

    fix

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-import css from './FeedbackOptions.module.css';
+// import css from './FeedbackOptions.module.css';
 
 const FeedbackOptions = ({ options, onLeaveFeedback }) => {
   return (
